29ca74d76ef1fa2bc19d8cd31a79d3ab50740ce6
gloriadazevedo <gloria.dazevedo@gmail.com>
2016-11-06 18:39:19 -0500

    Changed the script to run the R files for each of the type of constraint when they're ready

--- run.sh
+++ run.sh
@@ -4,4 +4,14 @@
 # but could be replaced with similar files from any major language
 
 # I'll execute my programs, with the input directory paymo_input and output the files in the directory paymo_output
-python ./src/antifraud.py ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
+#python ./src/antifraud.py ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
+
+#Gloria D'Azevedo submission using R
+#R script to yield output with the first type of constraint
+R CMD BATCH output_1.R
+
+#R script to yield output with the second type of constraint
+R CMD BATCH output_2.R
+
+#R script to yield output with the third type of constraint
+R CMD BATCH output_3.R
